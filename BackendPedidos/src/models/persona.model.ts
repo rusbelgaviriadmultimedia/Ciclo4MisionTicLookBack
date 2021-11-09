15b717ba0b694157d13e7f47114ee6916824423c
Rusbel Gaviria <kimar041604@gmail.com>
2021-11-08 23:22:08 -0500

    Model empresa

---
+++ BackendPedidos/src/models/persona.model.ts
@@ -0,0 +1,53 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id_documento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  TipoDocumento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Correo: string;
+
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
+
+export interface PersonaRelations {
+  // describe navigational properties here
+}
+
+export type PersonaWithRelations = Persona & PersonaRelations;
